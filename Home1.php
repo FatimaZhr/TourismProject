bace16b361c25411d7e91d2a7a3e7d5a2844b3db
FatimaZhr <125465069+FatimaZhr@users.noreply.github.com>
2024-11-24 00:35:31 +0100

    added a home controller

---
+++ Home1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Home1 extends BaseController
+{
+    public function index(): string
+    {
+    
+       return view('home/home1');
+    }
+}
+
+
+
+
+ 
